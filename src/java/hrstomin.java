5e479f507573a410e6e44263d190ba46304b78df
tejasumate118 <tejasumate118@gmail.com>
2023-08-17 15:48:11 +0530

    Java Begineer programe

---
+++ src/java/hrstomin.java
@@ -0,0 +1,10 @@
+public class hrstomin {
+    public static void main(String[] args) {
+        int hour= 4;
+        int min= 10;
+        int sechr= hour * 60 *60;
+        int secmin= min*60;
+        System.out.println("4hrs in sec is:" + sechr);
+        System.out.println("10Min in sec is:" + secmin);
+    }
+}
---
+++ src/java/increment.java
@@ -0,0 +1,14 @@
+public class increment {
+    public static void main(String[] args) {
+        int a=10;
+        int d=10;
+        //pre increment
+        int b= a++ + 10;
+        System.out.println(b);
+        //post increment
+        int c= ++d + 10;
+        System.out.println(c);
+
+    }
+
+}
