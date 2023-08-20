651dff07f379baae99e2966f9e720b7ea833ce9a
tejasumate118 <tejasumate118@gmail.com>
2023-08-20 10:22:15 +0530

    ValidParantheses

---
+++ src/java/array/ValidParantheses.java
@@ -0,0 +1,43 @@
+package array;
+
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ValidParantheses {
+
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type parantheses to validiate");
+        String s = sc.nextLine();
+        ValidParantheses validator = new ValidParantheses();
+
+        System.out.println(validator.isValid(s));
+    }
+
+    private boolean isValid(String s){
+        Stack<Character> stack =new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='('||c=='{'||c=='[') {
+                stack.push(c);
+            } else if (c==')'||c=='}'||c==']'){
+                if(stack.isEmpty()||!isMatchingPair(stack.pop(),c)){
+                    return false;
+                } }
+        }
+        return stack.isEmpty();
+    }
+    private boolean isMatchingPair(char open,char close){
+        return(open=='('&& close==')')||
+                (open=='['&& close==']')||
+                (open=='{'&& close=='}');
+
+    }
+
+}
+
+
