669907ad8106e449c178a8d80097dfbee8ca7885
tejasumate118 <tejasumate118@gmail.com>
2023-08-27 13:49:09 +0530

    Volume Of Box

---
+++ src/java/VolumeOfBox.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class VolumeOfBox {
+    public static void main(String[] args) {
+        Volume vol1 =new Volume();
+        double height,width,length;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Height, Width and Length of the box: ");
+        height= sc.nextDouble();
+        width= sc.nextDouble();
+        length= sc.nextDouble();
+        vol1.calculation(width,height,length);
+
+
+    }}
+class Volume {
+    public void calculation(double x, double y, double z) {
+        System.out.println("Volume is :" + (x*y*z));
+    }
+
+
+}
